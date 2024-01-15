235412a10294342a54aa31293bb0ca70d9a8054e
AshokYadav <yashoka51@gmail.com>
2024-01-15 15:25:00 +0530

    first commit

---
+++ components/meals/meals-grid.js
@@ -0,0 +1,10 @@
+import MealItem from './meal-item'
+import classes from './meals-grid.module.css'
+export default function MealsGrid({meals}){
+    // console.log(meals)
+    return <ul className={classes.meals}>
+        {meals.map(meal => <li key={meal.id}>
+            <MealItem {...meal}/>
+        </li>)}
+    </ul>
+}
